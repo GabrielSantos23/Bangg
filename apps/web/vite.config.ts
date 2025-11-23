cdcae7c8faf6f0f7b6d620cea8db53e4478aa5ff
gabriel <gabriel.gs605@gmail.com>
2025-11-23 16:00:55 -0300

    added some stuffs

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
@@ -14,6 +15,7 @@ export default defineConfig({
         },
       },
     }),
+    nitro(),
     react(),
     tailwindcss(),
   ],
